cd02f17fc6a2272711de90583e11c5877ac1533f
sonj tse <11626585@qq.com>
2018-11-06 11:33:35 +0800

    测试推送

---
+++ src/Kibiubiu/Queue.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: QiuXin
+ * Date: 2018/11/6
+ * Time: 11:29
+ */
+
+namespace Kibiubiu\Queue;
+
+class Queue {
+
+    public function start() {
+        echo '##############################################启动############################################';
+    }
+}
